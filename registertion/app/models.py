a904badfdf61c0e50ea258be9d0fb964148ce22a
harshs14 <harsh.singh1411@gmail.com>
2019-02-09 15:46:17 +0530

    setting models

---
+++ registertion/app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=True)
+    description = models.CharField(max_length=1000, null=False, blank=True)
+    avatar = models.ImageField(upload_to='event_pic')
+    video = models.FileField(upload_to='event_video')
+    timestamp = models.DateTimeField(default=None, null=True)
+    seats = models.IntegerField(null=False, blank=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Register(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=True)
+    phone_number = PhoneNumberField(blank=True, help_text='Contact phone number')
+    email = models.EmailField(null=False, blank=True)
+    address = models.CharField(null=False, blank=False, max_length=200)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=True)
+    email = models.EmailField(null=False, blank=True)
+    phone_number = PhoneNumberField(blank=True, help_text='Contact phone number')
+    enquiry = models.CharField(max_length=500, blank=False, null=True)
+
+    def __str__(self):
+        return str(self.id)
