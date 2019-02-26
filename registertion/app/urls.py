689435847562200cbe7ff3c378d1e804d8f4b4c0
harshs14 <harsh.singh1411@gmail.com>
2019-02-26 18:55:03 +0530

    event list and detail done

--- registertion/app/urls.py
+++ registertion/app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('event/', views.Events.as_view(), name='event_list'),
-    path('event/<int:pk>/', views.EventDetail.as_view(), name='event_detail'),
+    # path('event/(?P<e_id>[0-9]+)/', views.EventDetail.as_view(), name='event_detail'),
+    path('event/<int:pk>/', views.EventDetail.as_view(), name='detail'),
 ]
+
