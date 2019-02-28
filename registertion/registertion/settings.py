060f63a4465d10e054332515333f4eb93f0e06ef
harshs14 <harsh.singh1411@gmail.com>
2019-02-28 17:50:09 +0530

    working on hosting on heroku

--- registertion/registertion/settings.py
+++ registertion/registertion/settings.py
@@ -130,10 +130,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'onlineauction.si18@gmail.com'
 EMAIL_HOST_PASSWORD = 'onlineauctionhpi'
 EMAIL_PORT = 587
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
