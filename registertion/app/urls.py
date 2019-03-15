984ccde71fb6feb5de4842296b1e587d19718777
harshs14 <harsh.singh1411@gmail.com>
2019-03-15 10:49:33 +0530

    added few changes and working on bugs in regsiter

--- registertion/app/urls.py
+++ registertion/app/urls.py
@@ -2,8 +2,7 @@ from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
-    path('event/', views.Events.as_view(), name='event_list'),
-    path('event/<int:pk>/', views.EventDetail.as_view(), name='detail'),
-    path('event/<int:pk>/register', views.EventRegister.as_view(), name='register'),
+    re_path(r'^event/$', views.Events.as_view(), name='event_list'),
+    re_path(r'^event/(?P<e_id>[0-9]+)/$', views.EventDetail.as_view(), name='detail'),
+    re_path(r'^event/(?P<e_id>[0-9]+)/register/$', views.EventRegister.as_view(), name='register'),
 ]
-
