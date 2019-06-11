0b2a8765943c1eba8d7910e260b05c4d82aeaa37
harshs14 <harsh.singh1411@gmail.com>
2019-06-11 15:56:04 +0530

    env var setup

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+jiuvcf*py9s=s$za**41u8r1l$)1d(ao@m^w+rmc*4y#8!piw'
+SECRET_KEY = os.environ.get('secret_key_ceg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
