473013ea5b1044a30a2c761adbbf9a86a47b24ab
harshs14 <harsh.singh1411@gmail.com>
2019-06-15 16:24:07 +0530

    changes in wsgi

--- registration/wsgi.py
+++ registration/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "registration.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "registration.settings.development")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
