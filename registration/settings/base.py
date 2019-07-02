2c591d1485064cf4b634111ab46016bd00b1d5d3
harshs14 <harsh.singh1411@gmail.com>
2019-07-03 02:39:21 +0530

    fixes bugs

--- registration/settings/base.py
+++ registration/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('secret_key_ceg')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ceg-site.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ceg-reg.herokuapp.com']
 
 # Application definition
 
